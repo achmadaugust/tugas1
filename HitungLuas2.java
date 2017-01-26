8736eed0bc6c0760950412255bdd7859221d28c2
achmad <atryaugust@gmail.com>
2017-01-26 22:05:39 +0700

    penambahan method luas lingkaran

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -3,6 +3,12 @@ class HitungLuas2{
 	public static int cariLuasPersegiPanjang (int panjang, int lebar){
 		return panjang*lebar;
 	}
+	public static float cariLuasLingkaran (float r){
+		return (22*r*r)/7;
+	}
+	public static float cariLuasSegitiga (float alas, float tinggi){
+		return (alas*tinggi)/2;
+	}
 	public static void main (String []args){
 		Scanner x = new Scanner (System.in);
 		System.out.println ("Hitung Luas Persegi Panjang");
@@ -11,5 +17,15 @@ class HitungLuas2{
 		System.out.print ("Masukkan angka lebar = ");
 		int lebar = x.nextInt ();
 		System.out.println (panjang + " * " + lebar + " = " + cariLuasPersegiPanjang (panjang , lebar));
+		System.out.println ("Hitung Luas Lingkaran");
+		System.out.print ("Masukkan jari-jari = ");
+		float r = x.nextFloat ();
+		System.out.println ("( 22 * " + r + " * " + r + " ) / 7 = " + cariLuasLingkaran (r));
+		System.out.println ("Hitung Luas Segitiga");
+		System.out.print ("Masukkan angka alas = ");
+		float alas = x.nextFloat ();
+		System.out.print ("Masukkan angka tinggi = ");
+		float tinggi = x.nextFloat ();
+		System.out.println ("( " + alas + " * " + tinggi + " ) / 2 = " +cariLuasSegitiga (alas , tinggi));
 	}
 }
