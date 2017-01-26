4aa4e9d04c3385c94885e7282d1f35a9ef3ea439
achmad <atryaugust@gmail.com>
2017-01-26 18:02:30 +0700

    perubahan kedua

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
 class HitungLuas {
-	public static void main (String args[]){
+	public static int cariLuasPersegiPanjang (int panjang, int lebar){
+		return panjang*lebar;
+	}
+	public static void main (String []args){
+		Scanner x = new Scanner (System.in);
+		System.out.print ("Masukkan angka panjang = ");
+		int panjang = x.nextInt();
+		System.out.print ("Masukkan angka lebar = ");
+		int lebar = x.nextInt ();
+		System.out.println (panjang + " * " + lebar + " = " + cariLuasPersegiPanjang (panjang , lebar));
 	}
 }
